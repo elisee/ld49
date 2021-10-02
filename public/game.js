a724e7d0f99d99d184a04d02d65128e812fd9db5
Elisée Maurer <elisee@sparklinlabs.com>
2021-10-02 20:38:27 +0200

    Implement rails and basic gameplay

--- public/game.js
+++ public/game.js
@@ -6,13 +6,21 @@ import * as gameOver from "./gameOver.js";
 const canvasElt = $("canvas");
 const ctx = canvasElt.getContext("2d");
 
+canvasElt.addEventListener("pointerdown", onPointerDown);
+canvasElt.addEventListener("pointermove", onPointerMove);
+canvasElt.addEventListener("pointerup", onPointerUp);
+
 const startProgressValue = 5000;
 const maxProgressValue = 10000;
 
 const columnCount = 10;
+const rowCount = 10;
+const railStartRow = 5;
 const cellSize = 50;
 const boardSize = cellSize * 10;
 const fullSize = 600;
+const boardMargin = (fullSize - boardSize) / 2;
+let renderScale = 1;
 
 const data = {
   level: 0,
@@ -20,11 +28,14 @@ const data = {
   progress: { value: 0, decaySpeed: 0, lerpValue: 0 },
   totalTicks: 0,
   drops: [],
+  rails: [],
   spawnDropDelay: 0,
   gameOverFade: 0,
   badShake: 0,
   goodFlash: 0,
   levelUpFlash: 0,
+  hoveredRailIndex: -1,
+  drag: null,
 };
 
 export function enterGame() {
@@ -36,12 +47,27 @@ export function enterGame() {
   data.progress.value = startProgressValue;
   data.totalTicks = 0;
   data.drops.length = 0;
+  data.rails.length = 0;
   data.spawnDropDelay = 0;
   applyLevelProgress();
   data.gameOverFade = 0;
   data.badShake = 0;
   data.goodFlash = 0;
   data.levelUpFlash = 0;
+  data.hoveredRailIndex = -1;
+  data.drag = null;
+
+  let fill = 1500;
+  for (let i = 0; i < 4; i++) {
+    const offset = randomInt(0, 500);
+    const sections = [];
+    const sectionSize = randomInt(80, 160);
+    fill -= sectionSize;
+    sections.push([sectionSize, 0]);
+    data.rails.push({ offset, sections });
+  }
+
+  // TODO: distribute the remaining fill
 }
 
 function markGameOver() {
@@ -81,7 +107,7 @@ function animateGame(time) {
   ctx.fillRect(0, 0, width, height);
 
   const screenSize = Math.min(width, height);
-  const renderScale = screenSize / fullSize;
+  renderScale = screenSize / fullSize;
 
   ctx.save();
   ctx.translate(width / 2, height / 2);
@@ -89,7 +115,7 @@ function animateGame(time) {
   ctx.scale(renderScale, renderScale);
   ctx.translate(-fullSize / 2, -fullSize / 2);
 
-  ctx.fillStyle = "#45283c";
+  ctx.fillStyle = "#696a6a";
   ctx.fillRect(0, 0, fullSize, fullSize);
 
   ctx.textAlign = "center";
@@ -101,9 +127,8 @@ function animateGame(time) {
     ctx.fillRect(0, 0, fullSize, fullSize);
   }
 
-  const margin = (fullSize - boardSize) / 2;
   ctx.save();
-  ctx.translate(margin, margin);
+  ctx.translate(boardMargin, boardMargin);
 
   if (data.badShake > 0) {
     const angle = randomFloat(0, Math.PI * 2);
@@ -119,7 +144,7 @@ function animateGame(time) {
     ctx.translate(-boardSize / 2, -boardSize / 2);
   }
 
-  ctx.fillStyle = "#222034";
+  ctx.fillStyle = "#3f3f74";
   ctx.fillRect(0, 0, boardSize, boardSize);
   ctx.restore();
 
@@ -130,6 +155,26 @@ function animateGame(time) {
   ctx.lineTo(0, boardSize);
   ctx.closePath();
   ctx.clip();
+
+  if (data.hoveredRailIndex !== -1) {
+    ctx.save();
+    ctx.translate(0, (railStartRow + data.hoveredRailIndex) * cellSize);
+    ctx.fillStyle = "rgba(255, 255, 255, 10%)";
+    ctx.fillRect(0, 0, boardSize, cellSize);
+    ctx.restore();
+  }
+
+  ctx.fillStyle = "#222034";
+  for (const [i, rail] of data.rails.entries()) {
+    ctx.save();
+    ctx.translate(rail.offset, (i + railStartRow) * cellSize);
+    for (const [size, space] of rail.sections) {
+      ctx.fillRect(0, 2, size, cellSize - 4);
+      ctx.fillRect(-boardSize, 2, size, cellSize - 4);
+      ctx.translate(size + space, 0);
+    }
+    ctx.restore();
+  }
 
   ctx.fillStyle = "#fff";
   for (const drop of data.drops) {
@@ -148,21 +193,21 @@ function animateGame(time) {
 
   const levelBarHeight = 50;
   ctx.fillStyle = "#323c39";
-  ctx.fillRect(margin, fullSize - levelBarHeight, boardSize, levelBarHeight);
+  ctx.fillRect(boardMargin, fullSize - levelBarHeight, boardSize, levelBarHeight);
 
   const valueX = boardSize * data.progress.value / maxProgressValue
   const lerpValueX = boardSize * data.progress.lerpValue / maxProgressValue;
 
   if (lerpValueX < valueX) {
     ctx.fillStyle = "#fbf236";
-    ctx.fillRect(margin, fullSize - levelBarHeight, lerpValueX, levelBarHeight);
+    ctx.fillRect(boardMargin, fullSize - levelBarHeight, lerpValueX, levelBarHeight);
     ctx.fillStyle = "#37946e";
-    ctx.fillRect(margin + lerpValueX, fullSize - levelBarHeight, valueX - lerpValueX, levelBarHeight);
+    ctx.fillRect(boardMargin + lerpValueX, fullSize - levelBarHeight, valueX - lerpValueX, levelBarHeight);
   } else {
     ctx.fillStyle = "#fbf236";
-    ctx.fillRect(margin, fullSize - levelBarHeight, valueX, levelBarHeight);
+    ctx.fillRect(boardMargin, fullSize - levelBarHeight, valueX, levelBarHeight);
     ctx.fillStyle = "#ac3232";
-    ctx.fillRect(margin + valueX, fullSize - levelBarHeight, lerpValueX - valueX, levelBarHeight);
+    ctx.fillRect(boardMargin + valueX, fullSize - levelBarHeight, lerpValueX - valueX, levelBarHeight);
   }
 
   const progressText = `${(data.progress.value / 100).toFixed(1)}%`;
@@ -175,7 +220,8 @@ function animateGame(time) {
 
   ctx.textAlign = "left";
   const levelText = `LVL ${data.level + 1}`;
-  ctx.fillText(levelText, margin, margin / 2);
+  ctx.strokeText(levelText, boardMargin, boardMargin / 2);
+  ctx.fillText(levelText, boardMargin, boardMargin / 2);
 
   if (data.levelUpFlash > 0) {
     ctx.fillStyle = `rgba(255, 255, 255, ${data.levelUpFlash / 4}%)`;
@@ -224,6 +270,20 @@ function tick() {
 
   data.progress.lerpValue = lerp(data.progress.lerpValue, data.progress.value, 0.25);
 
+  if (data.drag != null) {
+    const rail = data.rails[data.hoveredRailIndex];
+    rail.offset = data.drag.startOffset - data.drag.startX + data.drag.currentX;
+    while (rail.offset > boardSize) rail.offset -= boardSize;
+    while (rail.offset < 0) rail.offset += boardSize;
+  }
+
+  function rectsIntersect(r1, r2) {
+    return !(r2.left > r1.right ||
+      r2.right < r1.left ||
+      r2.top > r1.bottom ||
+      r2.bottom < r1.top);
+  }
+
   for (const drop of data.drops) {
     if (drop.collected) {
       drop.collectPop = Math.max(0, drop.collectPop - 25);
@@ -233,6 +293,26 @@ function tick() {
       else data.goodFlash = 100;
       drop.collected = true;
       drop.collectPop = 100;
+    } else {
+      const dropRect = new DOMRect(drop.x - 10, drop.y - 10, 20, 20);
+
+      for (const [i, rail] of data.rails.entries()) {
+        const railY = (railStartRow + i) * cellSize;
+        let sectionX = rail.offset;
+        for (const [size, space] of rail.sections) {
+          const railRect1 = new DOMRect(sectionX, railY, size, cellSize);
+          const railRect2 = new DOMRect(sectionX - boardSize, railY, size, cellSize);
+
+          if (rectsIntersect(dropRect, railRect1) || rectsIntersect(dropRect, railRect2)) {
+            drop.collected = true;
+            drop.collectPop = 100;
+            break;
+          }
+
+          sectionX += space;
+        }
+        if (drop.collected) break;
+      }
     }
   }
   data.drops = data.drops.filter(x => !x.collected || x.collectPop > 0);
@@ -245,7 +325,7 @@ function tick() {
 }
 
 function spawnDrop() {
-  data.spawnDropDelay = randomInt(20, 50);
+  data.spawnDropDelay = randomInt(10, 30);
 
   const isBonus = randomInt(0, 2) <= 1;
   const bonuses = [
@@ -284,4 +364,46 @@ function applyLevelProgress() {
   data.badShake = 0;
   data.goodFlash = 0;
   data.levelUpFlash = 100;
+}
+
+function updateHoveredRail(pointerY) {
+  const railIndex = Math.floor((pointerY / renderScale - boardMargin) / cellSize) - railStartRow;
+  if (railIndex >= 0 && railIndex < data.rails.length) {
+    data.hoveredRailIndex = railIndex;
+  } else {
+    data.hoveredRailIndex = -1;
+  }
+}
+
+function updateCursor() {
+  canvasElt.style.cursor = data.hoveredRailIndex !== -1 ? (data.drag != null ? "grabbing" : "grab") : "";
+}
+
+function onPointerDown(event) {
+  if (!event.isPrimary) return;
+
+  updateHoveredRail(event.y);
+  if (data.hoveredRailIndex === -1) return;
+
+  const rail = data.rails[data.hoveredRailIndex];
+
+  const gameX = event.x / renderScale;
+  data.drag = { startOffset: rail.offset, startX: gameX, currentX: gameX };
+  updateCursor();
+}
+
+function onPointerMove(event) {
+  if (!event.isPrimary) return;
+
+  if (data.drag == null) {
+    updateHoveredRail(event.y);
+    updateCursor();
+  } else {
+    data.drag.currentX = event.x / renderScale;
+  }
+}
+
+function onPointerUp(event) {
+  if (!event.isPrimary) return;
+  data.drag = null;
 }
